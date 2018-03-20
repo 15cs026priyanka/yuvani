b99323269cce579f8f2b62dcd1d143b086f69d71
15cs026priyanka <priyankaappu1998@gmail.com>
2018-03-20 15:21:56 +0530

    Create count.py

---
+++ count.py
@@ -0,0 +1,9 @@
+import stringdef count_letters(word):
+    global count
+    wordsList = string.split(word)
+    count = Counter()
+    for words in wordsList:
+        for letters in set(words):
+            return count[letters]
+word = "i likes driving "
+print count_letters(word)
